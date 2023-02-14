e0fdab7ce8e1c30bd115391c076c6fab3fb3d69b
BK ^^ <84545556+Barikeloo@users.noreply.github.com>
2023-02-14 15:29:46 +0100

    subiendo el script atotal.sh

---
+++ atotal.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+while true; do
+  echo "Introduzca la ciudad (Valencia, Barcelona o Madrid) en minusculas porfavor :) = "
+  read ciudad
+  consumo_total=0
+  while IFS=' ' read -r ciudad_actual mes_actual anyo_actual consumo_actual; do
+    if [[ $ciudad_actual == $ciudad ]]; then
+      consumo_total=$((consumo_total + consumo_actual))
+    fi
+  done < archivo.txt
+
+  if [[ $consumo_total -eq 0 ]]; then
+    echo "La ciudad introducida no es válida, inténtelo de nuevo."
+  else
+    echo "El consumo total de $ciudad en 2022 fue de $consumo_total."
+    break
+  fi
+done
